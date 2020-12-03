497fd46dbc0df222a24e715747ddb679c274c07c
Yang Keao <keao.yang@yahoo.com>
2020-12-03 15:33:02 +0800

    add support for other namespace
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>

--- nsenter/src/lib.rs
+++ nsenter/src/lib.rs
@@ -1,17 +1,18 @@
-use nix::fcntl::open;
-use nix::fcntl::OFlag;
+use nix::fcntl::{open, OFlag};
 use nix::sys::stat::Mode;
+use nix::sched::{setns, CloneFlags};
 
 use std::path::PathBuf;
 
 #[ctor::ctor]
 #[no_mangle]
 unsafe fn nsenter() {
-    let pid = std::env::var("__MNTEXEC_PID").unwrap();
-    let pid: i32 = pid.parse().unwrap();
+    if let Ok(pid) = std::env::var("__MNTEXEC_PID") {
+        let pid: i32 = pid.parse().unwrap();
 
-    let fd = open(&PathBuf::from(format!("/proc/{}/ns/mnt", pid)), OFlag::O_RDONLY, Mode::empty()).unwrap();
-    let ret = libc::setns(fd, libc::CLONE_NEWNS);
+        let fd = open(&PathBuf::from(format!("/proc/{}/ns/mnt", pid)), OFlag::O_RDONLY, Mode::empty()).unwrap();
+        setns(fd, CloneFlags::CLONE_NEWNS).unwrap();
 
-    println!("SET MNT NS {}", ret);
+        std::env::remove_var("LD_PRELOAD")
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use structopt::StructOpt;
 
+use nix::sched::{CloneFlags, setns};
+use nix::fcntl::{open, OFlag};
+use nix::sys::stat::Mode;
+
 use std::process::Command;
+use std::path::PathBuf;
 
 #[derive(StructOpt, Debug)]
 #[structopt(
@@ -8,25 +13,89 @@ use std::process::Command;
 )]
 struct Opt {
     #[structopt(short, long)]
-    pid: i32,
+    target: i32,
 
-    #[structopt(short, long, default_value="/lib/libnsenter.so")]
+    #[structopt(short, long)]
+    cgroup: bool,
+
+    #[structopt(short, long)]
+    ipc: bool,
+
+    #[structopt(short, long)]
+    mnt: bool,
+
+    #[structopt(short, long)]
+    net: bool,
+
+    #[structopt(short, long)]
+    pid: bool,
+
+    #[structopt(short, long)]
+    user: bool,
+
+    #[structopt(short, long)]
+    uts: bool,
+
+    #[structopt(short, long, default_value="/usr/local/lib/libnsenter.so")]
     library_path: String,
 
     #[structopt(required = true)]
-    pub extra: Vec<String>
+    pub cmd: Vec<String>
 }
 
 fn main() {
     let opts = Opt::from_args();
 
-    let mut command = Command::new(&opts.extra[0]);
-    if opts.extra.len() > 1 {
-        command.args(&opts.extra[1..]);
+    if opts.cgroup {
+        let fd = open(&PathBuf::from(format!("/proc/{}/ns/cgroup", opts.target)), OFlag::O_RDONLY, Mode::empty()).unwrap();
+        setns(fd, CloneFlags::CLONE_NEWCGROUP).unwrap();
     }
 
-    command.env("LD_PRELOAD", opts.library_path);
-    command.env("__MNTEXEC_PID", format!("{}", opts.pid));
+    if opts.ipc {
+        let fd = open(&PathBuf::from(format!("/proc/{}/ns/ipc", opts.target)), OFlag::O_RDONLY, Mode::empty()).unwrap();
+        setns(fd, CloneFlags::CLONE_NEWIPC).unwrap();
+    }
+
+    if opts.net {
+        let fd = open(&PathBuf::from(format!("/proc/{}/ns/net", opts.target)), OFlag::O_RDONLY, Mode::empty()).unwrap();
+        setns(fd, CloneFlags::CLONE_NEWNET).unwrap();
+    }
+
+    if opts.pid {
+        let fd = open(&PathBuf::from(format!("/proc/{}/ns/pid", opts.target)), OFlag::O_RDONLY, Mode::empty()).unwrap();
+        setns(fd, CloneFlags::CLONE_NEWPID).unwrap();
+    }
+
+    if opts.user {
+        let fd = open(&PathBuf::from(format!("/proc/{}/ns/user", opts.target)), OFlag::O_RDONLY, Mode::empty()).unwrap();
+        setns(fd, CloneFlags::CLONE_NEWUSER).unwrap();
+    }
+
+    if opts.uts {
+        let fd = open(&PathBuf::from(format!("/proc/{}/ns/uts", opts.target)), OFlag::O_RDONLY, Mode::empty()).unwrap();
+        setns(fd, CloneFlags::CLONE_NEWUTS).unwrap();
+    }
+
+    let mut command = if opts.mnt {
+        let mut command = Command::new("/lib64/ld-linux-x86-64.so.2");
+        
+        let cmd = opts.cmd.iter().map(|s| s.as_str());
+        let args: Vec<&str> = ["--preload", &opts.library_path].iter().map(|s| *s).chain(cmd).collect();
+        command.args(args);
+
+        command.env("__MNTEXEC_PID", format!("{}", opts.target));
+
+        command
+    } else {
+        let mut command = Command::new(&opts.cmd[0]);
+        if opts.cmd.len() > 1 {
+            command.args(&opts.cmd[1..]);
+        }
+
+        command
+    };
+
+    
     let mut child = command.spawn().unwrap();
     child.wait().unwrap();
 }
